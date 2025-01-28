a1632c67f9329dce2c2fcf95bd1c8280f4f3cdfb
Andrew Meisenbacher <andrew.meisenbacher@kellogg.northwestern.edu>
2025-01-28 19:58:18 +0000

    updated in class

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -4,16 +4,53 @@
 
 # In Ruby, there are different *types* of data:
 
-# Numbers
+# # Numbers
+# puts 2
+# puts 5
 
-# Non-numbers
+# puts 5+2
+# puts 5-2
+# puts 5/2.0
+# puts 5*2
+# puts 5^2
+
+# # Non-numbers
+
+# puts 'Hello, world!'
+# puts 'Tacos are ' +'delish'
+# puts 'Tacos ' *3
+# puts "tacos" + 3.to_s
 
 # True, False
 
+puts 1 == 1+1
+puts 1 < 1+1
+
 # Nothing
+
+puts nil
 
 # Variables
 
+food = "tacos"
+x = 3
+
+puts food * x
+
 # Combine strings and variables
 
+puts "tacos: #{x}"
+
+puts "#{food}: #{x}"
+
 # String manipulation
+puts "Hello".length
+puts "how are you today?".capitalize
+puts "computer".reverse
+puts "racecar".reverse
+
+creed="This Is The Way"
+
+puts creed.upcase
+puts creed.downcase
+puts creed.swapcase
