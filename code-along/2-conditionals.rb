0a35474ed362af640b02e80b90abc5b9bef9151d
Andrew Meisenbacher <andrew.meisenbacher@kellogg.northwestern.edu>
2025-01-28 20:31:20 +0000

    did in class

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -4,12 +4,66 @@
 
 # Booleans
 
-# Boolean Expressions
+# test_is_true = true
+# puts test_is_true
 
-# If Conditional Logic
+# test_is_false = false
+# puts test_is_false
+
+# # Boolean Expressions
+
+# puts 3 == 2
+# puts 3 != 2
+# puts 3>2
+# puts 3<2
+
+# # If Conditional Logic
+
+# if 3==2
+#     puts "This text shouldn't display"
+# end    
+
+# if 3>2
+#     puts "This text should always display"
+# end
 
 # If/Else Conditional Logic
 
-# Elsif Conditional Logic
+# if 3 == 2 
+#     puts "This text shouldn't display"
+# else
+#     puts "This text should always display"
+# end 
+
+# user_entered_password ="tacos"
+# real_password="secret"
+
+# if user_entered_password = real_password
+#     puts "Your password matched. Welcome back!"
+# else
+#     puts "Wrong passwword! Try again!"
+# end
+
+# # Elsif Conditional Logic
+
+# your_team_score =2
+# other_team_score =2
+
+# if your_team_score > other_team_score
+#     puts "You won!"
+# elsif your_team_score = other_team_score
+#     puts "Tied!"
+# else
+#     puts "You lost!"
+# end
 
 # Combining Expressions
+
+temp = 75
+precipitation = 0
+
+if temp >= 65 && temp <=75 && precipitation==0
+    puts "It's perfect outside"
+end
+
+
