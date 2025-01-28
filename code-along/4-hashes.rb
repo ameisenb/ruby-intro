aced53dc4df5741b67b25942d3775a2a5975bf57
Andrew Meisenbacher <andrew.meisenbacher@kellogg.northwestern.edu>
2025-01-28 21:09:30 +0000

    hashes in class

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -4,6 +4,34 @@
 
 # Hashes are lists of *key-value pairs*
 
+me = {"name" => "Andrew", 'location' => "Chicago", 'status'=> 'MBA student'}
+p me
+
 # Accessing data from the hash
 
+location = me ['location']
+p location
+
 # More Complex Hashes
+my_profile = {
+    "name"=> "Andrew",
+    'location'=> {"city"=>'Chicago', "state"=> "Illinois"},
+    'status'=> "Kellogg MBA"
+}
+
+p my_profile
+p my_profile ['location']['city']
+
+my_profile['name']= "Andrew Meisenbacher"
+p my_profile
+
+complete_profile = {'name'=> 'Andrew Meisenbacher',
+'location'=> {"city"=>'Chicago', "state"=> "Illinois"},
+'timeline'=>[{'status'=> 'Eating tacos', 'posted'=> "7:30am"},
+{'status'=> 'Brushing teeth', 'posted'=> '8:30am'},
+{'status'=> 'Eating more tacos', 'posted'=> '9:30am'}]
+}
+
+p complete_profile
+
+p complete_profile['timeline'][0]['status']
